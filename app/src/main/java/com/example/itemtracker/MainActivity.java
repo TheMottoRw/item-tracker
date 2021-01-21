f3cfbdbbfdf2e8066cced2cdaf9e0bec761bc260
igorjabiro <201850180@uaeu.ac.ae>
2021-01-21 12:12:26 +0400

    declaration form added

--- app/src/main/java/com/example/itemtracker/MainActivity.java
+++ app/src/main/java/com/example/itemtracker/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.itemtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    public FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fab = findViewById(R.id.fab_decla);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),declaration_form.class));
+            }
+        });
     }
 }
